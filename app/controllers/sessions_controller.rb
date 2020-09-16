a6704539c18b1ab207d330e65e3a513b761825dc
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-09-16 16:19:34 +0100

    Add destroy method to session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,20 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by_name(params[:id])
-    session[:user] = user.id
+    if user    
+      session[:user] = user.id
+      flash[:success] = 'Sign In Successfully'
+      redirect_to root_path
+    else
+      flash.now[:alert] = 'User does not Exist, Sign up please!!'
+      render :new
+    end
+  end
+
+
+  def destroy
+    session[:user] = nil
+    flash[:success] = "Signed Out Successfully"
+    redirect_to root_path
   end
 end
