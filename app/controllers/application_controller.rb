45e01a68cb9a1e28cb15c6bbec795b72e9c5b497
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-09-16 15:42:14 +0100

    Add new user form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+    def user_params
+      params.require(:user).permit(:name)
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,24 @@
 class UsersController < ApplicationController
   def index
-    @user = User.all
+    @users = User.all
   end
 
   def new
-     
+     @user = User.new
   end
 
   def create
-     
+    @user = User.new(user_params)
+    
+    if @user.save
+    redirect_to root_path  
+    else
+      render :new      
+    end
+    
   end
 
   def show
-    
+    @user = User.find(params[:id])  
   end
-  
 end
