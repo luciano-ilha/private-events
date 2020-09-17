87370d33140be5922af3e5bdaaae5b4a9d641492
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-09-17 14:19:25 +0100

    Add routes to contorller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:new, :create, :show]
   resources :sessions
+  resources :events, except: [:edit,:destroy]
   root 'users#index'
 
    get '/login' => 'sessions#new'
