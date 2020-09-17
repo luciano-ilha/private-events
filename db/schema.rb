c16c4fb6aded341d21cda6004446dd953c8dfe0d
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-09-17 12:21:29 +0100

    Add refrrences to creator

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_17_105153) do
+ActiveRecord::Schema.define(version: 2020_09_17_111908) do
 
   create_table "events", force: :cascade do |t|
     t.date "event_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "creator_id"
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
