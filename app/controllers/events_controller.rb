f0234e1a79f8b5648291c4ee9b06865f22115444
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-09-17 14:28:42 +0100

    Add views to event controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,18 @@
 class EventsController < ApplicationController
+
+    def index
+        @users = User.all
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
 end
