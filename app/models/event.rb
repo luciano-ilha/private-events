3bea46f1d80e07d99e18cb2c34217e1ac42a9b1b
Luciano Ilha Carbonell <ilha.dev.br@gmail.com>
2020-09-23 10:10:08 -0300

    Refactor upcoming and past events in event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,11 +4,14 @@ class Event < ApplicationRecord
     has_many :event_attendances, foreign_key: :attended_event_id
     has_many :attendees, through: :event_attendances, source: :attendee
 
-    def self.upcoming      
-        where("event_date > ?", Time.now )
-    end
+    scope :upcoming, -> { where("event_date > ?", Time.now) }
+    scope :past, -> { where("event_date < ?", Time.now) }
 
-    def self.past      
-        where("event_date < ?", Time.now )
-    end
+    # def self.upcoming      
+    #     where("event_date > ?", Time.now )
+    # end
+
+    # def self.past      
+    #     where("event_date < ?", Time.now )
+    # end
 end
