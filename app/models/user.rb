04de37dbb3e5e25695ce252a47a19d878f6ea858
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-09-24 14:40:23 +0100

    Add Upcoming and Past to Creaed and Attended Event user show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,4 @@ class User < ApplicationRecord
   has_many :attended_events, through: :event_attendances
 
   validates :name, presence: true
-
-  scope :upcoming, -> { where('event_date > ?', Time.now) }
-  scope :past, -> { where('event_date < ?', Time.now) }
 end
