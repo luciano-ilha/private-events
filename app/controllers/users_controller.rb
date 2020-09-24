51310c6a10b17d3cb9eb31a16366ba5cdac82731
Haroonabdulrazaq <Haroonabdulrazaq@gmail.com>
2020-09-24 16:35:04 +0100

    Change the signup redirection path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to root_path
+      redirect_to new_session_path
     else
       render :new
     end
